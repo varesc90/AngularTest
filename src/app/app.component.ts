c2041ce0b899ccbcb5f1abc7c183741b88a0c734
varesc <varesc@gmail.com>
2017-10-28 15:34:55 +0700

    add routing to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,90 +1,19 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-    id: number;
-    name: string;
-}
 
-const HEROES: Hero[] = [
-    {id: 11, name: 'Superman'},
-    {id: 12, name: 'Spiderman'},
-    {id: 13, name: 'Batman'},
-    {id: 14, name: 'The Flash'},
-    {id: 15, name: 'Dare Devil'},
-    {id: 16, name: 'Thor'},
-    {id: 17, name: 'Quick Silver'},
-    {id: 18, name: 'Captain America'},
-    {id: 19, name: 'Iron Man'},
-    {id: 20, name: 'Vision'}
-];
 
 @Component({
-    styles: [`
-        .selected {
-            background-color: #CFD8DC !important;
-            color: white;
-        }
-        .heroes {
-            margin: 0 0 2em 0;
-            list-style-type: none;
-            padding: 0;
-            width: 15em;
-        }
-        .heroes li {
-            cursor: pointer;
-            position: relative;
-            left: 0;
-            background-color: #EEE;
-            margin: .5em;
-            padding: .3em 0;
-            height: 1.6em;
-            border-radius: 4px;
-        }
-        .heroes li.selected:hover {
-            background-color: #BBD8DC !important;
-            color: white;
-        }
-        .heroes li:hover {
-            color: #607D8B;
-            background-color: #DDD;
-            left: .1em;
-        }
-        .heroes .text {
-            position: relative;
-            top: -3px;
-        }
-        .heroes .badge {
-            display: inline-block;
-            font-size: small;
-            color: white;
-            padding: 0.8em 0.7em 0 0.7em;
-            background-color: #607D8B;
-            line-height: 1em;
-            position: relative;
-            left: -1px;
-            top: -4px;
-            height: 1.8em;
-            margin-right: .8em;
-            border-radius: 4px 0 0 4px;
-        }
-    `],
-    selector: 'app-root',
-    template: `<h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-        <li *ngFor="let hero of heroes">
-            <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-    </ul>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>`,
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <a routerLink="/heroes">Heroes</a>
+        <router-outlet></router-outlet>`
 })
-export class AppComponent {
+
+
+export class AppComponent
+{
+
     title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
-    heroes = HEROES;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import {HeroService} from './hero.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-      FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent, HeroDetailComponent, HeroesComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            }
+        ])
+    ],
+    providers: [HeroService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
